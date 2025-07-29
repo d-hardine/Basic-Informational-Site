5ba0fd5508f1fcdd6470d027687bdff3c9b6a5ab
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-07-29 10:08:10 +0700

    javascript fixed, small edit on the contact-me site

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 import { createServer } from 'node:http';
+import { readFile } from 'node:fs';
 
 const hostname = 'localhost';
-const port = 3000;
+const port = 8080;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    let filePath = ''
+    let statusCode = 200
+    let contentType = 'text/html'
+
+    if (req.url === "/")
+        filePath = "index.html";
+    else if (req.url === "/about")
+        filePath = "about.html";
+    else if (req.url === "/contact-me")
+        filePath = "contact-me.html";
+    else {
+        filePath = "404.html";
+        statusCode = 404;
+    }
+    console.log(filePath)
+
+    readFile(filePath, (err, data) => {
+        if(err) {
+            res.statusCode = 500
+            res.setHeader('content-type', 'text/plain')
+            res.end('Internal Server Error\n')
+            return
+        }
+
+        res.statusCode = statusCode
+        res.setHeader('content-type', contentType)
+        res.end(data)
+    })
 });
 
 server.listen(port, hostname, () => {
