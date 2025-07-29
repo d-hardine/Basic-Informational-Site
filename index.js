6ef1bdeb50d898454d9f071218ee9e87300b66d0
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-07-29 22:27:56 +0700

    install express package, rewrite the code for using express method

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//pure node.js, without express. uncomment if necessary
+/*
 import { createServer } from 'node:http';
 import { readFile } from 'node:fs';
 
@@ -19,7 +21,6 @@ const server = createServer((req, res) => {
         filePath = "404.html";
         statusCode = 404;
     }
-    console.log(filePath)
 
     readFile(filePath, (err, data) => {
         if(err) {
@@ -36,5 +37,23 @@ const server = createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
+
+//using express, comment the whole codes below and uncomment codes above to use the pure node.js version
+import express from 'express'
+import path from 'path'
+const __dirname = import.meta.dirname; //ESM is not recognize __dirname variable, this is the solution
+
+const app = express()
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")))
+app.get("/about", (req, res) => res.sendFile(path.join(__dirname, "about.html")))
+app.get("/contact-me", (req, res) => res.sendFile(path.join(__dirname, "contact-me.html")))
+app.use((req, res) => res.status(404).sendFile(path.join(__dirname, '404.html')))
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
